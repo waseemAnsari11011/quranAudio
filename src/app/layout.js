f7bee6b3b52ef3bf2f10c1c8179a3545643aa91c
waseemAnsari11011 <waseemahm11011@gmail.com>
2025-09-22 17:35:44 +0530

    abc

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,23 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Quran Audio - Listen to the Holy Quran",
   description:
-    "A modern, ad-free platform for listening to the Holy Quran with verse-by-verse recitations and tafsir.",
+    "Explore the Quran with high-quality audio recitations, translations, and tafsir from various Qaris. An ad-free and accessible platform for a beautiful Quran listening experience.",
+  openGraph: {
+    title: "Quran Audio - Listen & Understand the Holy Quran",
+    description:
+      "Immerse yourself in the words of the Quran. Listen to beautiful recitations, explore different Qaris, and understand the meaning with translation and tafsir for a beautiful experience. Click to start your spiritual journey.",
+    url: "https://quranaudio.in",
+    images: [
+      {
+        url: "https://quranaudio.in/quran-audio-og-image.png", // Correct absolute URL
+        width: 1200,
+        height: 630,
+        alt: "Quran Audio - Listen & Reflect",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
